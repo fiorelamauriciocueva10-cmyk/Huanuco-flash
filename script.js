c38500b4c31e8719bb8a501521f0eca547b2932a
fiorelamauriciocueva10-cmyk <fiorelamauriciocueva10@gmail.com>
2026-04-21 01:38:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 document.getElementById("pedidoForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const cliente = document.getElementById("cliente").value;
-    const telefono = document.getElementById("telefono").value;
-    const recojo = document.getElementById("recojo").value;
-    const entrega = document.getElementById("entrega").value;
-    const referencia = document.getElementById("referencia").value;
-    const pedido = document.getElementById("pedido").value;
-    const costo = document.getElementById("costo").value;
-    const pago = document.getElementById("pago").value;
-    const obs = document.getElementById("obs").value;
-    const motorizado = document.getElementById("motorizado").value;
-
     const mensaje = `\u{1F680} *NUEVO PEDIDO - HUÁNUCO FLASH* \u{1F680}
+
+\u{1F464} Cliente: ${cliente}
+\u{1F4DE} Teléfono: ${telefono}
+
+\u{1F4CD} Recojo: ${recojo}
+
+\u{1F4CD} Entrega: ${entrega}
+\u{1F4CC} Referencia: ${referencia}
+
+\u{1F4E6} Pedido: ${pedido}
+
+\u{1F4B0} Costo: ${costo}
+\u{1F4B3} Pago: ${pago}
+
+\u{1F4DD} Obs: ${obs}`;
 
 \u{1F464} Cliente: ${cliente}
 \u{1F4DE} Teléfono: ${telefono}
