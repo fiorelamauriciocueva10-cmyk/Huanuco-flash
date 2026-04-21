1176fced41151b2230402ec4a3c3afed8080b59b
fiorelamauriciocueva10-cmyk <fiorelamauriciocueva10@gmail.com>
2026-04-21 01:40:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,30 @@
 document.getElementById("pedidoForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const mensaje = `\u{1F680} *NUEVO PEDIDO - HUÁNUCO FLASH* \u{1F680}
+    const cliente = document.getElementById("cliente").value;
+    const telefono = document.getElementById("telefono").value;
+    const recojo = document.getElementById("recojo").value;
+    const entrega = document.getElementById("entrega").value;
+    const referencia = document.getElementById("referencia").value;
+    const pedido = document.getElementById("pedido").value;
+    const costo = document.getElementById("costo").value;
+    const pago = document.getElementById("pago").value;
+    const obs = document.getElementById("obs").value;
+    const motorizado = document.getElementById("motorizado").value;
 
-\u{1F464} Cliente: ${cliente}
-\u{1F4DE} Teléfono: ${telefono}
+    const mensaje = 
+    "NUEVO PEDIDO - HUANUCO FLASH\n\n" +
+    "Cliente: " + cliente + "\n" +
+    "Teléfono: " + telefono + "\n\n" +
+    "Recojo: " + recojo + "\n\n" +
+    "Entrega: " + entrega + "\n" +
+    "Referencia: " + referencia + "\n\n" +
+    "Pedido: " + pedido + "\n\n" +
+    "Costo: " + costo + "\n" +
+    "Pago: " + pago + "\n\n" +
+    "Obs: " + obs;
 
-\u{1F4CD} Recojo: ${recojo}
-
-\u{1F4CD} Entrega: ${entrega}
-\u{1F4CC} Referencia: ${referencia}
-
-\u{1F4E6} Pedido: ${pedido}
-
-\u{1F4B0} Costo: ${costo}
-\u{1F4B3} Pago: ${pago}
-
-\u{1F4DD} Obs: ${obs}`;
-
-\u{1F464} Cliente: ${cliente}
-\u{1F4DE} Teléfono: ${telefono}
-
-\u{1F4CD} Recojo: ${recojo}
-
-\u{1F4CD} Entrega: ${entrega}
-\u{1F4CC} Referencia: ${referencia}
-
-\u{1F4E6} Pedido: ${pedido}
-
-\u{1F4B0} Costo: ${costo}
-\u{1F4B3} Pago: ${pago}
-
-\u{1F4DD} Obs: ${obs}`;
-
-    const url = "https://wa.me/" + motorizado + "?text=" + encodeURIComponent(unescape(encodeURIComponent(mensaje)));
+    const url = "https://wa.me/" + motorizado + "?text=" + encodeURIComponent(mensaje);
 
     window.open(url, "_blank");
 });
